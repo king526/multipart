3fc53d5ba855496310d4d23e7b6c258edc019d77
king526 <1988526dl@163.com>
2018-12-11 17:51:31 +0800

    init

---
+++ example_test.go
@@ -0,0 +1,22 @@
+// Copyright 2014 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package multipart_test
+
+import (
+	"fmt"
+	"testing"
+
+	"cindasoft.com/library/multipart"
+)
+
+func Test_1(t *testing.T) {
+	b := multipart.NewFormBody()
+	fmt.Println(b.WriteField("f1", "v1"))
+	fmt.Println(b.WriteField("f2", "v2"))
+	fmt.Println(b.CreateFromByPath("f4", "f44", "example_test.go"))
+	fmt.Println(b.WriteField("f3", "v3"))
+
+	b.Print()
+}
---
+++ formbody.go
@@ -0,0 +1,171 @@
+package multipart
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/textproto"
+	"os"
+	"sort"
+)
+
+type eofReader struct{}
+
+func (eofReader) Read([]byte) (int, error) {
+	return 0, io.EOF
+}
+
+type multiReader struct {
+	readers []io.Reader
+}
+
+func (mr *multiReader) Read(p []byte) (n int, err error) {
+	for len(mr.readers) > 0 {
+		// Optimization to flatten nested multiReaders (Issue 13558).
+		if len(mr.readers) == 1 {
+			if r, ok := mr.readers[0].(*multiReader); ok {
+				mr.readers = r.readers
+				continue
+			}
+		}
+		n, err = mr.readers[0].Read(p)
+		if err == io.EOF {
+			// Use eofReader instead of nil to avoid nil panic
+			// after performing flatten (Issue 18232).
+			mr.readers[0] = eofReader{} // permit earlier GC
+			mr.readers = mr.readers[1:]
+		}
+		if n > 0 || err != io.EOF {
+			if err == io.EOF && len(mr.readers) > 0 {
+				// Don't return EOF yet. More readers remain.
+				err = nil
+			}
+			return
+		}
+	}
+	return 0, io.EOF
+}
+
+type FormBody struct {
+	boundary string
+	b        *bytes.Buffer
+	items    []io.Reader
+	reader   *multiReader
+}
+
+func NewFormBody() *FormBody {
+	b := &FormBody{
+		b:        bytes.NewBuffer(nil),
+		boundary: randomBoundary(),
+	}
+	return b
+}
+
+func (w *FormBody) Print() {
+	bs, err := ioutil.ReadAll(w)
+	fmt.Println(string(bs), err)
+}
+
+func (w *FormBody) Read(p []byte) (n int, err error) {
+	if len(w.items) == 0 {
+		return 0, io.EOF
+	}
+	if w.reader == nil {
+		fmt.Fprintf(w.b, "\r\n--%s--\r\n", w.boundary)
+		w.items = append(w.items, w.b)
+		w.reader = &multiReader{w.items}
+	}
+	return w.reader.Read(p)
+}
+
+// Boundary returns the Writer's boundary.
+func (w *FormBody) Boundary() string {
+	return w.boundary
+}
+
+func (w *FormBody) FormDataContentType() string {
+	return "multipart/form-data; boundary=" + w.boundary
+}
+
+func (w *FormBody) Close() error {
+	//fmt.Fprintf(w.w, "\r\n--%s--\r\n", w.boundary)
+	return nil
+}
+
+func (f *FormBody) getPart() bool {
+	if f.b.Len() != 0 {
+		raw := make([]byte, f.b.Len())
+		copy(raw, f.b.Bytes())
+		f.items = append(f.items, bytes.NewReader(raw))
+		f.b.Reset()
+		return true
+	}
+	return false
+}
+
+func (f *FormBody) CreatePart(header textproto.MIMEHeader) (io.Writer, error) {
+	if f.getPart() {
+		fmt.Fprintf(f.b, "\r\n--%s\r\n", f.boundary)
+	} else {
+		fmt.Fprintf(f.b, "--%s\r\n", f.boundary)
+	}
+	keys := make([]string, 0, len(header))
+	for k := range header {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+	for _, k := range keys {
+		for _, v := range header[k] {
+			fmt.Fprintf(f.b, "%s: %s\r\n", k, v)
+		}
+	}
+	fmt.Fprintf(f.b, "\r\n")
+	f.getPart()
+	return f.b, nil
+
+}
+
+func (w *FormBody) CreateFromByPath(fieldname, filename, filePath string) error {
+	fd, err := os.Open(filePath)
+	if err != nil {
+		return err
+	}
+	return w.CreateFromReader(fieldname, filename, fd)
+}
+
+func (w *FormBody) CreateFromReader(fieldname, filename string, rd io.ReadCloser) error {
+	if _, err := w.CreateFormFile(fieldname, filename); err != nil {
+		return err
+	}
+	w.items = append(w.items, rd)
+	return nil
+}
+
+func (w *FormBody) CreateFormFile(fieldname, filename string) (io.Writer, error) {
+	h := make(textproto.MIMEHeader)
+	h.Set("Content-Disposition",
+		fmt.Sprintf(`form-data; name="%s"; filename="%s"`,
+			escapeQuotes(fieldname), escapeQuotes(filename)))
+	h.Set("Content-Type", "application/octet-stream")
+	return w.CreatePart(h)
+}
+
+// CreateFormField calls CreatePart with a header using the
+// given field name.
+func (w *FormBody) CreateFormField(fieldname string) (io.Writer, error) {
+	h := make(textproto.MIMEHeader)
+	h.Set("Content-Disposition",
+		fmt.Sprintf(`form-data; name="%s"`, escapeQuotes(fieldname)))
+	return w.CreatePart(h)
+}
+
+// WriteField calls CreateFormField and then writes the given value.
+func (w *FormBody) WriteField(fieldname, value string) error {
+	p, err := w.CreateFormField(fieldname)
+	if err != nil {
+		return err
+	}
+	_, err = p.Write([]byte(value))
+	return err
+}
